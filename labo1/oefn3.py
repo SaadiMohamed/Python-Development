3bb160a9045d03ee4a4ac6069f63bd61d6fb0644
SaadiMohamed <s125317@ap.be>
2022-02-09 14:48:25 +0100

    Vooruitgang

---
+++ labo1/oefn3.py
@@ -0,0 +1,4 @@
+naam = input("Wat is je naam?:")
+intresse = input("In wat ben je geintreseerd?")
+
+print(f"Hallo {naam}, ik zie dat je geintreseerd bent door {intresse}")
---
+++ labo1/oefn4.py
@@ -0,0 +1,4 @@
+lengte = float(input("Wat is de lengte van je kamer (in meter)?: "))
+breedte = float(input("Wat is de breedte van je kamer (in meter)?: "))
+
+print(f"De oppervlakte van je kamer is {(lengte * breedte)} m²")
